b647288929c1ea9dd46504d15b0b9ddc383f9cef
Your Name <you@example.com>
2025-11-30 18:00:16 +0400

    this is standart commit task

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """this is comment"""
-    return list(mongo_collection.find({"topic": topic }))
+    return list(mongo_collection.find({"topics": topic }))
