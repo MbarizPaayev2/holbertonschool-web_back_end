7b98b622d21b5bee46e9af9c43b16643a9ec0c5a
Your Name <you@example.com>
2025-11-30 17:51:04 +0400

    this is standart commit task

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,5 +6,5 @@ def update_topics(mongo_collection, name, topics):
     """this is docstr"""
     mongo_collection.update_many(
         {"name":name},
-        {"$set" { "topics":}}
+        {"$set" { "topics": topics }}
     )
